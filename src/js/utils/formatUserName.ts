01f520259dfd192e8cdd408839665fa86e0fe142
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-03-20 21:46:38 +0100

    Remove format user name function

--- src/js/utils/formatUserName.ts
+++
@@ -1,4 +0,0 @@
-export const formatUserName = (user: any) => {
-    if (!user) return "";
-    return user?.firstName && user?.lastName ? `${user.firstName} ${user.firstName}` : user?.username || "";
-};
