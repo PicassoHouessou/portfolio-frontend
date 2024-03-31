fd0f9afc1f30fe6a0cecc9ca22cc24b1bc88448d
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-03-31 13:00:18 +0200

    Remove axios

--- src/composables/api.ts
+++ src/composables/api.ts
@@ -1,6 +1,4 @@
 import type {UseFetchOptions} from "#app";
-import type {KeysOf} from "#app/composables/asyncData";
-import axios from "axios";
 export const useApi = async <T>(url:any, options?: UseFetchOptions<T> | undefined) => {
     const store =  useLocaleStore()
     const config = useRuntimeConfig();
@@ -20,47 +18,3 @@ export const useApi = async <T>(url:any, options?: UseFetchOptions<T> | undefine
 
     });
 }
-
-export const downloadMyCv = (url:string, locale="en", label?:string) => {
-
-    useApi
-
-    axios.get(url + "?lang=" + unref(locale), {responseType: "blob"})
-        .then((response) => {
-            const blob = new Blob([response.data], {type: "application/pdf"});
-            const link = document.createElement("a");
-            link.href = URL.createObjectURL(blob);
-            link.download = label ? label: "Mon CV";
-            link.click();
-            URL.revokeObjectURL(link.href);
-            // this.$Progress.finish();
-        })
-        .catch((error) => {
-            console.log(error);
-            // this.$Progress.finish()
-        });
-
-
-}
-
-
-
-
-/*
-export function $api<T>(
-    request: Parameters<typeof $fetch<T>>[0],
-    opts?: Parameters<typeof $fetch<T>>[1],
-) {
-    const auth = useAuth()
-
-    return $fetch<T>(request, {
-        ...opts,
-        headers: {
-            Authorization: auth.logged ? `Bearer ${auth.accessToken}` : '',
-            ...opts?.headers,
-        },
-    })
-}
-
-
- */
